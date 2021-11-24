2da37580a3d905e6a92b211996780240ef03f3f4
zion <lce322@naver.com>
2021-11-25 00:20:16 +0900

    BinaryTreeInorderTraversal_094

--- algorithm/src/algorithm/BinaryTreeInorderTraversal_094.java
+++ algorithm/src/algorithm/BinaryTreeInorderTraversal_094.java
@@ -11,44 +11,49 @@ import model.TreeNode;
 /**
  * 
  * @author zion 
- * Runtime: 1 ms, faster than 26.99% of Java online submissions for Merge Two Sorted Lists.
- * Memory Usage: 39.4 MB, less than 27.11% of Java online submissions for Merge Two Sorted Lists.
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Binary Tree Inorder Traversal.
+ * Memory Usage: 38.5 MB, less than 26.78% of Java online submissions for Binary Tree Inorder Traversal.
  *
  */
 public class BinaryTreeInorderTraversal_094 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x = 1534236469; // output: bab
-		//ListNode temp;
-		TreeNode tn = new TreeNode(1,null,new TreeNode(2,new TreeNode(3),null));
+		// ListNode temp;
+		TreeNode tn = new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null));
 
 		List<Integer> result_1 = inorderTraversal(tn);
-		
-		for(Integer i : result_1) {
+
+		for (Integer i : result_1) {
 			System.out.println(i);
-        }
+		}
 	}
 
-    public static List<Integer> inorderTraversal(TreeNode root) {
-    	List<Integer> result = new LinkedList<Integer>();
-    	Queue<TreeNode> list = new LinkedList<TreeNode>();
-    	TreeNode temp = root;
-    	while(temp!= null) {
-	    	while(temp.left!= null) {
-	    		list.add(temp);//중위
-	    		temp = temp.left;
-	    	}
-	    	result.add(temp.val);
-	    	if(temp.right== null) {
-	    		if(list.isEmpty()) {return result;}
-	    		temp = list.poll();//중위
-	    		result.add(temp.val);
-	    	}
-	    	temp=temp.right;
+	static List<Integer> result = new LinkedList<Integer>();
 
-    	}
-        return result;
-    }
+	public static List<Integer> inorderTraversal(TreeNode root) {
+        result = new LinkedList<Integer>();
+        if(root != null) {
+		    traversal(root);
+        }
+		return result;
+	}
+
+	public static void traversal(TreeNode root) {
+
+		if (root.left == null && root.right == null) {
+			result.add(root.val);
+			return;
+		}
+		if (root.left != null) {
+			traversal(root.left);
+		}
+		result.add(root.val);
+		if (root.right != null) {
+			traversal(root.right);
+		}
+
+		return;
+	}
 
 }
