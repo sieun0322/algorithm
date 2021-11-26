4ce1816942e8f4770a326d8c9d672c63fcc42e5f
zion <lce322@naver.com>
2021-11-27 02:47:08 +0900

    RegularExpressionMatching_010 FAIL

--- algorithm/src/algorithm/RegularExpressionMatching_010.java
+++ algorithm/src/algorithm/RegularExpressionMatching_010.java
@@ -11,57 +11,67 @@ public class RegularExpressionMatching_010 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String S = "mississippi"; // output: bab
+
 		String P = "mis*is*ip*."; // output: bab
-		System.out.println(isMatch(S,P));
+		String S = "mississippi"; // output: bab
+		System.out.println(isMatch(S, P));
 	}
 
-    public static boolean isMatch(String s, String p) {
-    	try {
-	        int p_idx = 0;
-	        char compareChar;
-	        for(int idx = 0; idx<s.length(); idx++){
-	            compareChar = p.charAt(p_idx);
-	            System.out.println(p_idx+":"+idx);
-	            System.out.println(compareChar+":"+s.charAt(idx));
-	            if(compareChar == '*'||compareChar == '.' || compareChar== s.charAt(idx)){
-	            	if(compareChar == '*'){
-	            		if(p.charAt(p_idx-1) != '.' && p.charAt(p_idx-1) != s.charAt(idx)) {
-		                	p_idx++;
-		                	if(idx == s.length()-1) {
-		                		compareChar = p.charAt(p_idx);
-		                		if(compareChar == '.' || compareChar== s.charAt(idx)){
-		                			return true;
-		                		}
-		                	}
-		                }else if(p.charAt(p_idx-1) == '.'){
-	            			
-	            		}
-		            }else {
-		            	p_idx++;
-		            }
-	            }else{
-	            	if(idx<s.length()-2) {
-	            		if(p.charAt(p_idx+1) == '*') {
-	            			p_idx++;
-	            			idx--;
-	            		}else {
-	            			return false;
-	            		}
-	            	}else {
-	            		return false;
-	            	}
-	            }
-	            System.out.println("idx:"+idx);
-	            System.out.println("p_idx:"+p_idx);
-	            System.out.println("============");
-	            if(idx ==s.length()-1 && p_idx > p.length()-1) {
-	            	return true;
-	            }
-	        }
-    	}catch(Exception e) {
-    		return false;
-    	}
-        return true;
-    }
+	public static boolean isMatch(String s, String p) {
+		try {
+			int p_idx = 0;
+			char compareChar;
+			for (int idx = 0; idx < s.length(); idx++) {
+				compareChar = p.charAt(p_idx);
+
+				System.out.println("============");
+				System.out.println(p_idx + ":" + compareChar);
+				System.out.println(idx + ":" + s.charAt(idx));
+				if (compareChar == '*') {
+					compareChar = p.charAt(p_idx-1);
+					if (compareChar == s.charAt(idx)) {
+						
+					} else if (compareChar == '.') {
+						
+					} else {
+						p_idx++;
+						if (idx == (s.length() - 1) && p_idx == (p.length() - 1)
+								&& s.charAt(idx)==p.charAt(p_idx)) {
+							return true;
+						}
+					}
+				} else if (compareChar == '.') {
+					p_idx++;
+				} else if (compareChar == s.charAt(idx)) {
+					p_idx++;
+				} else {
+					if(p_idx<p.length()-1) {
+						if(p.charAt(p_idx+1) == '*') {
+							idx--;
+							p_idx++;
+						}else {
+							return false;
+						}
+					}else {
+						return false;
+					}
+				}
+				System.out.println(p_idx + "=" + (p.length()));
+				System.out.println(idx + "=" + (s.length() - 1));
+				if (idx == (s.length() - 1)) {
+					
+					while(p_idx<p.length()) {
+						if(p.charAt(p_idx) !='*') {
+							return false;
+						}
+						p_idx++;
+					}
+					return true;
+				}
+			}
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
 }
