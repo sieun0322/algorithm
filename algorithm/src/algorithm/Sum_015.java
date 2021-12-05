396b20047a38fcf9854d921c069195757cf183e2
zion <lce322@naver.com>
2021-12-05 16:03:02 +0900

    3Sum_015 commit

--- algorithm/src/algorithm/Sum_015.java
+++ algorithm/src/algorithm/Sum_015.java
@@ -12,7 +12,8 @@ import java.util.Stack;
 /**
  * 
  * @author zion 
- * 
+ * Runtime: 255 ms, faster than 26.40% of Java online submissions for 3Sum.
+ * Memory Usage: 43.9 MB, less than 37.66% of Java online submissions for 3Sum.
  *
  */
 public class Sum_015 {
@@ -29,7 +30,6 @@ public class Sum_015 {
 		
 		Arrays.sort(nums);
         int plusStartIndex = -1;
-        //int plusEndIndex = -1;
         
 		for(int i = 0; i<nums.length; i++) {
             if(nums[i] > 0){
@@ -44,55 +44,31 @@ public class Sum_015 {
 		for(int i = 0; i<plusStartIndex; i++) {
             int i_num = nums[i];
             
-            int startIndex = -1;
-            int endIndex = -1;
+            int startIndex = i+1;
+            int endIndex = nums.length -1;
             
-			for(int j = i+1; j<nums.length-1; j++) {
-                int j_num = nums[j];
-                if(i_num + j_num < 0){
-                    startIndex = j+1<plusStartIndex?plusStartIndex:j+1;
-                    endIndex = nums.length;
-                }else if(i_num + j_num == 0){
-                    if(j+1>plusStartIndex-1){
-                        break;
-                    }
-                    startIndex = plusStartIndex-1;
-                    endIndex = startIndex+1;
-                }else{
-                    break;
-                }
-				for(int k = startIndex; k<endIndex; k++) {
-                    int k_num = nums[k];
-                    if(i_num+j_num+k_num>0) break;
-					if(i_num+j_num+k_num == 0) {
-						temp = new ArrayList<Integer>();
-						temp.add(i_num);
-						temp.add(j_num);
-						temp.add(k_num);
-						temp.sort(null);
-						result.add(temp);
-                        break;
-					}
-                    if(nums[0] == nums[nums.length-1]) return new ArrayList(result);
-				}
-			}
+            while(startIndex<endIndex) {
+                int j_num = nums[startIndex];
+                int k_num = nums[endIndex];
+                int sum = i_num+j_num+k_num;
+                
+	            if(sum == 0) {
+					temp = new ArrayList<Integer>();
+					temp.add(i_num);
+					temp.add(j_num);
+					temp.add(k_num);
+					temp.sort(null);
+					result.add(temp);
+                    
+                    startIndex++;
+                    endIndex--;
+				}else if(sum > 0) {
+					endIndex--;
+				} else if(sum < 0) {
+					startIndex++;
+				} 
+            }
 		}
 		return new ArrayList(result);
     }
-	public static boolean compare(List<List<Integer>> list,List<Integer> t ) {
-		if(list.isEmpty()) return true;
-		for(int i = 0; i<list.size(); i++) {
-			List<Integer> row = list.get(i);
-			System.out.print(row.get(0)+":"+t.get(0)+",");
-			System.out.print(row.get(1)+":"+t.get(1)+",");
-			System.out.print(row.get(2)+":"+t.get(2)+",");
-			if(row.get(0)==t.get(0) && row.get(1)==t.get(1)&& row.get(2)==t.get(2)) {
-				return false;
-			}else {
-				return true;
-			}
-		}
-		return true;
-	}
-
 }
