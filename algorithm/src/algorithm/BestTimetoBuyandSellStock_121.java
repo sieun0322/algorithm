52cc3a8b81996bac35d00a5cb101a3e3a407fc89
zion <lce322@naver.com>
2022-06-21 02:20:19 +0900

    BestTimetoBuyandSellStock_121 ACCEPTED

--- algorithm/src/algorithm/BestTimetoBuyandSellStock_121.java
+++ algorithm/src/algorithm/BestTimetoBuyandSellStock_121.java
@@ -4,60 +4,41 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 /**
- * LeetCode 1. Two Sum
- * Easy
+ * 
  * @author zion
- *
+ * Runtime: 2534 ms, faster than 5.29% of Java online submissions for Best Time to Buy and Sell Stock.
+ * Memory Usage: 177.2 MB, less than 5.15% of Java online submissions for Best Time to Buy and Sell Stock.
  */
 public class BestTimetoBuyandSellStock_121 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//int[] prices = {7,1,5,3,6,4};
+		int[] prices = {7,1,5,3,6,4};
 		//int[] prices = {7,6,4,3,1};
 		//int[] prices = {1,2};
-		int[] prices = {1,2,4};
-		
-		int[] sortNum = Arrays.copyOf(prices, prices.length);
-		Arrays.sort(sortNum);
-		int maxNum = sortNum[sortNum.length-1];
-		int minNum = sortNum[0];
+		//int[] prices = {1,2,4};
 		
 		
 		int result = 0;
-		int buyPrice = prices[0];
-		
-		if(prices.length<2) {
-			result=0;
-		}else if(prices.length==2) {
-			result=prices[0]<prices[1]?prices[1]-prices[0]:0;
-		}else {
-			for(int idx = 0; idx<prices.length; idx++) {
-				if(result>prices[idx]-minNum) {
-					continue;
-				}else if(result>prices[idx]) {
-					continue;
-				}else {
-					int tempMax = bestTimetoBuyandSellStock(prices,idx);
-						result = result <= tempMax ?tempMax:result;
-						buyPrice = result <= tempMax ?prices[idx]:buyPrice;
-				}
-			}
-		}
-		System.out.println("result:"+result);
-						
+        int max = -1;
+        int minNum = 10000;
+        boolean maxChange = true;
+        
+        for(int idx = 0; idx<prices.length-1; idx++) {
+            if(prices[idx] == max){
+                maxChange = true;
+            }
+            if(prices[idx] >= minNum){
+                continue;
+            }
+            minNum = prices[idx] <= minNum ?prices[idx]:minNum;
+            if(maxChange){
+                int[] arr = Arrays.copyOfRange(prices, idx+1, prices.length);
+                max = Arrays.stream(arr).max().getAsInt();  
+                maxChange = false;
+            }
+            result = result <= max-minNum ? max-minNum:result;
+        }
+		System.out.println("result:"+result);				
 	}
-	public static int bestTimetoBuyandSellStock(int[] prices,int buyIdx) {
-		int max = 0;
-		for(int idx = buyIdx+1; idx<prices.length; idx++) {
-			if(prices[buyIdx] > prices[idx]) {
-				continue;
-			}else {
-				max = max <=prices[idx] - prices[buyIdx]?prices[idx] - prices[buyIdx]:max;
-				
-			}
-			
-		}
-        return max;
-    }
 }
