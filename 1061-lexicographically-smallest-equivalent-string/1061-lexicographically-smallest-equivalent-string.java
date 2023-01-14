dded2003e192de9c5b2e7a93cc5760c99bafe307
zion <lce322@naver.com>
2023-01-14 18:55:25 +0900

    Time: 5 ms (48.86%), Space: 42.2 MB (66.48%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.java
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.java
@@ -1,77 +1,42 @@
 class Solution {
-    public String smallestEquivalentString(String s1, String s2, String baseStr) {
-        HashMap<Character, HashMap<Character, Boolean>> m = new HashMap<>();
-
-        HashMap<Character, Boolean> curMap = new HashMap();
-        Character saveKey;
-        Boolean chkYn = false;
-        for(int cIdx = 0; cIdx<s1.length(); cIdx++){
-            
-            Character c1 =s1.charAt(cIdx);
-            Character c2 =s2.charAt(cIdx);
-                    
-            chkYn = false;
-            List<Character> keyList = new ArrayList<>(m.keySet());
-            keyList.sort((char1, char2) -> char1.compareTo(char2));
-            for(Character key : keyList){ //저장된 key값 확인
-                curMap = m.get(key);
-                if(curMap.containsKey(c1)&&curMap.containsKey(c2)){
-                        chkYn =true;
-                        break;
-                    }else if(curMap.containsKey(c1)){
-                        chkYn = addCharacter(m,key,c2);
-                        break;
-                    }
-                    else if(curMap.containsKey(c2)){
-                        chkYn = addCharacter(m,key,c1);
-                        break;
-                    }
-                
-            }
-            if(!chkYn){
-                curMap = new HashMap();
-                saveKey = s1.charAt(cIdx)<s2.charAt(cIdx)?s1.charAt(cIdx):s2.charAt(cIdx);
-                curMap.put(s1.charAt(cIdx),true);
-                curMap.put(s2.charAt(cIdx),true);
-                m.put(saveKey,curMap);
-            }
+    public String smallestEquivalentString(String A, String B, String S) {
+        Map<Character, Character> letterGroupMap = new HashMap<Character, Character>();
+        Map<Character, Set<Character>> groupLettersMap = new HashMap<Character, Set<Character>>();
+        for (char c = 'a'; c <= 'z'; c++) {
+            letterGroupMap.put(c, c);
+            Set<Character> set = new HashSet<Character>();
+            set.add(c);
+            groupLettersMap.put(c, set);
         }
-        String result="";
-        
-        for(int cIdx = 0; cIdx<baseStr.length(); cIdx++){
-            chkYn = false;
-            List<Character> keyList = new ArrayList<>(m.keySet());
-            keyList.sort((char1, char2) -> char1.compareTo(char2));
-            for(char key : keyList){ //저장된 key값 확인
-                curMap = m.get(key);
-                if(curMap.containsKey(baseStr.charAt(cIdx))){
-                        result+=key;
-                        chkYn = true;
-                        break;
-                }
-            }
-            if(!chkYn){
-                result+=baseStr.charAt(cIdx);
-            }
-        }
-        return result;
-    }
-    public static boolean addCharacter(HashMap<Character, HashMap<Character, Boolean>> m,Character key,Character input){
-        
-        HashMap<Character, Boolean> curMap = m.get(key);
-        curMap.put(input,true);
-
-        for(Character tempK : m.keySet()){ //저장된 key값 확인
-            if(tempK !=key ) {
-                HashMap<Character, Boolean> temp = m.get(tempK);
-                if(temp.containsKey(input)){
-                    curMap.putAll(temp);
+        int length = A.length();
+        for (int i = 0; i < length; i++) {
+            char c1 = A.charAt(i), c2 = B.charAt(i);
+            char group1 = letterGroupMap.get(c1), group2 = letterGroupMap.get(c2);
+            if (group1 != group2) {
+                Set<Character> set1 = groupLettersMap.get(group1);
+                Set<Character> set2 = groupLettersMap.get(group2);
+                if (group1 < group2) {
+                    for (char c : set2)
+                        letterGroupMap.put(c, group1);
+                    set1.addAll(set2);
+                    groupLettersMap.put(group1, set1);
+                    groupLettersMap.remove(group2);
+                } else {
+                    for (char c : set1)
+                        letterGroupMap.put(c, group2);
+                    set2.addAll(set1);
+                    groupLettersMap.put(group2, set2);
+                    groupLettersMap.remove(group1);
                 }
             }
         }
-        Character saveKey = input<key?input:key;
-        m.remove(key);
-        m.put(saveKey,curMap);
-        return true;
+        char[] array = S.toCharArray();
+        int strLength = array.length;
+        for (int i = 0; i < strLength; i++) {
+            char c = array[i];
+            char group = letterGroupMap.get(c);
+            array[i] = group;
+        }
+        return new String(array);
     }
 }
